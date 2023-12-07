9eff811bcd53947f522a18068125c7ec596a5c76
amruta05 <153206723+amruta05@users.noreply.github.com>
2023-12-07 21:36:27 +0530

    Update pp.py

--- pp.py
+++ pp.py
@@ -2,3 +2,7 @@ import streamlit as st
 st.title("Simple addition calculator")
 def addition(a,b):
   return a+b
+a = st.number_input("first")
+b = st.number_input("second")
+
+st.write('this is a : ',a)
